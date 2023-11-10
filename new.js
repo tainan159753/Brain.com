f7fa84820479b685be9af5652c6dfc04cb8cfd1f
tainan159753 <145068606+tainan159753@users.noreply.github.com>
2023-11-10 19:15:39 -0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,11 @@
+/* Js do HEADER */
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    if (window.scrollY > 1) { // Quando você rolar mais de 100 pixels
+      header.classList.add("header-fixed"); // Adicione a classe para tornar o cabeçalho fixo
+    } else {
+      header.classList.remove("header-fixed"); // Remova a classe para retornar ao posicionamento normal
+    }
+  });
+  
